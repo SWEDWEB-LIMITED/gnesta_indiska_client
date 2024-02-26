d832a40b8e2a0d96b404c3cf550da86c26a7bd6b
Shammo06 <mrshammo2018@gmail.com>
2024-02-26 23:57:55 +0600

    add banner

---
+++ src/Pages/Home/Banner/Banner.tsx
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import banner from "../../../assets/home/banner.jpg";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+const Banner = () => {
+  return (
+    <>
+       <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='' src={banner} alt="" /></SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default Banner;
