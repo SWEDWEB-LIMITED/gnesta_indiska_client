8e2eee60b31f0044fcd6d018a57daad65db77110
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-02-27 01:21:25 +0600

    issues solved

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,11 +2,9 @@ import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Footer from "./Footer/Footer";
 
-
 const Home = () => {
   return (
     <div>
-      This is home page
       <Banner></Banner>
       <Category></Category>
       <Footer></Footer>
