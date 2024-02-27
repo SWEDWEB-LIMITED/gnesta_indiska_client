f16cacc71ffa437ccbc9dd4b619f90330acfbdbf
keplar-404 <shehabhossen76.services@gmail.com>
2024-02-27 14:23:25 +0000

    shehub-recepy and google map added bug fixes

--- src/Pages/Home/LocationGoogleMap/LocationGoogleMap.tsx
+++ src/Pages/Home/LocationGoogleMap/LocationGoogleMap.tsx
@@ -1,10 +1,13 @@
+// LocationGoogleMap component
 import GoogleMap from "../../../Component/GoogleMap";
 
 export default function LocationGoogleMap() {
   return (
-    <div className="shehub_center_div flex-col mt-[6rem]">
-      <h1 className="text-4xl font-bold mb-[6rem]">Our location</h1>
-      <GoogleMap width="100" height="12" />
-    </div>
+    <>
+      <h1 className="text-4xl font-bold my-[6rem] text-center">Our location</h1>
+      <div className="w-full">
+        <GoogleMap width="100" height="170" />
+      </div>
+    </>
   );
 }
