db0caab890552670257dfb150883c4f399546074
Shammo06 <mrshammo2018@gmail.com>
2024-02-27 15:04:54 +0600

    fixed banner carousel

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -5,15 +5,15 @@ import banner from "../../../assets/home/banner.jpg";
 
 const Banner = () => {
     return (
-        <Carousel className="text-center" autoPlay={true} infiniteLoop={true} interval={5000} >
+        <Carousel className="text-center" autoPlay={true} infiniteLoop={true} interval={5000}  >
             <div>
-                <img src={banner} alt="Banner 1" />
+                <img src={banner} className='max-h-[550px]'  alt="Banner 1" />
             </div>
             <div>
-                <img src={banner} alt="Banner 2" />
+                <img src={banner} className='max-h-[550px]' alt="Banner 2" />
             </div>
             <div>
-                <img src={banner} alt="Banner 3" />
+                <img src={banner} className='max-h-[550px]' alt="Banner 3" />
             </div>
         </Carousel>
     );
